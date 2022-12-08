21110f8017d2fce49080993a066530307446eb4e
mahsamabbas <s.mahsamabbas@gmail.com>
2022-12-08 15:18:17 +0500

    Optimization of code.

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Services\UserValidation;
+use App\Http\Requests\UserLoginValidation;
+use App\Http\Requests\UserRegisterValidation;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\JsonResponse;
@@ -16,21 +17,11 @@ use Symfony\Component\HttpFoundation\Response;
 class AuthController extends Controller
 {
     /**
-     * @param Request $request
-     * @param UserValidation $userValidation
+     * @param UserLoginValidation $request
      * @return Application|ResponseFactory|\Illuminate\Http\Response
      */
-    public function login(Request $request, UserValidation $userValidation)
+    public function login(UserLoginValidation $request)
     {
-        $userValidation = $userValidation->validateUserForRegister($request);
-
-        if ($userValidation->fails()){
-            return response()->json([
-                'error' => $userValidation->getMessageBag(),
-                'status' => Response::HTTP_UNAUTHORIZED
-            ], Response::HTTP_OK);
-
-        }
         if (auth()->attempt($request->all())) {
             return response([
                 'user' => auth()->user(),
@@ -47,20 +38,11 @@ class AuthController extends Controller
     }
 
     /**
-     * @param Request $request
-     * @param UserValidation $userValidation
+     * @param UserRegisterValidation $request
      * @return JsonResponse
      */
-    public function register(Request $request, UserValidation $userValidation)
+    public function register(UserRegisterValidation $request)
     {
-        $userValidation = $userValidation->validateUserForLogin($request);
-
-        if ($userValidation->fails()){
-            return response()->json([
-                'error' => $userValidation->getMessageBag(),
-                'status' => Response::HTTP_UNAUTHORIZED
-            ], Response::HTTP_OK);
-        }
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
